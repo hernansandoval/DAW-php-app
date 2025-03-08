8e14714a76301baa2a1e764fb1e54c8f9765c161
hernansandoval <127244360+hernansandoval@users.noreply.github.com>
2025-03-08 18:29:30 +0100

    Documentació afegida a nou.php

--- nou.php
+++ nou.php
@@ -1,4 +1,21 @@
 <?php
-echo "Hola, mundo! soy main";
-echo "Hola, mundo! soy develop";
+/**
+ * Script nou.php
+ *
+ * Aquest script imprimeix un missatge per pantalla.
+ *
+ * @author [Tu Nombre]
+ * @version 1.0
+ * @package DAW-php-app
+ */
+
+/**
+ * Mostra un missatge per pantalla
+ */
+function mostrarMissatge() {
+    echo "Hola des de l'aplicació!";
+}
+
+// Executar la funció
+mostrarMissatge();
 ?>
